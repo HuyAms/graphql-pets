3377ee94b89730c44599b2506553f6a4b36d5595
Huy Trinh <htrinh@alpha-sense.com>
2024-09-04 17:28:34 +0300

    feat: remove useIndexSignature from the codegen - not type safe

--- codegen.ts
+++ codegen.ts
@@ -3,9 +3,6 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  config: {
-    useIndexSignature: true
-  },
   schema: "src/schema.graphql",
   generates: {
     "src/__generated__/graphql.ts": {
